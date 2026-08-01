be77c01dd1adfd8c457e36f3e0c6a1d799b3b531
Abhishek-22ds01 <Abhishek98739999@gmail.com>
2026-08-01 14:01:55 +0530

    Implemented JWT login authentication

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -3,8 +3,17 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.user import User
-from app.schemas.auth import UserSignup, UserResponse
-from app.security import hash_password
+from app.schemas.auth import (
+    UserSignup,
+    UserResponse,
+    UserLogin,
+    Token,
+)
+from app.security import (
+    hash_password,
+    verify_password,
+    create_access_token,
+)
 
 router = APIRouter(
     prefix="/auth",
@@ -44,3 +53,47 @@ def signup(
     db.refresh(new_user)
 
     return new_user
+
+
+
+# LOGIN API 
+
+@router.post(
+    "/login",
+    response_model=Token,
+)
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db),
+):
+    db_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password",
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password,
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password",
+        )
+
+    access_token = create_access_token(
+        data={
+            "sub": db_user.email,
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+    }
